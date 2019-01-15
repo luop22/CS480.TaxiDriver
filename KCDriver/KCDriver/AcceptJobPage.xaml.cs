943bbb8aab0896adf067f1ce469243619786c462
Dave <gamino1996@live.com>
2019-01-14 18:30:40 -0800

    Additional Pages added

---
+++ KCDriver/KCDriver/AcceptJobPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KCDriver
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AcceptJobPage : ContentPage
+	{
+		public AcceptJobPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
