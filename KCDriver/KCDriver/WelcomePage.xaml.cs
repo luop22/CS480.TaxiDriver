7ffd5358b67284d77f5fe8a39a87a702f0cc2434
Dave <gamino1996@live.com>
2019-01-15 20:27:53 -0800

    Included images and other features

--- KCDriver/KCDriver/WelcomePage.xaml.cs
+++ KCDriver/KCDriver/WelcomePage.xaml.cs
@@ -12,30 +12,11 @@ namespace KCDriver
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class WelcomePage : ContentPage
 	{
-        Image splashImage;
 
         public WelcomePage()
         {
-            //InitializeComponent ();
+            InitializeComponent ();
             NavigationPage.SetHasNavigationBar(this, false);
-
-            var sub = new AbsoluteLayout();
-
-            splashImage = new Image
-            {
-                Source = "cab.PNG",
-                WidthRequest = 200,
-                HeightRequest = 200
-            };
-
-            AbsoluteLayout.SetLayoutFlags(splashImage, AbsoluteLayoutFlags.PositionProportional);
-            AbsoluteLayout.SetLayoutBounds(splashImage,
-                new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
-
-            sub.Children.Add(splashImage);
-
-            this.BackgroundColor = Color.FromHex("#000000");
-            this.Content = sub;
         }
 
         protected override async void OnAppearing()
