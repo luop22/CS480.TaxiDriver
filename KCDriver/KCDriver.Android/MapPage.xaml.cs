07a5ef5ac7af84aba61a731b96fba1f4504704cf
jacobdavis1 <jacob.marquez.davis@hotmail.com>
2019-02-21 17:56:15 -0800

    More commits for error flow.

--- KCDriver/KCDriver.Android/MapPage.xaml.cs
+++ KCDriver/KCDriver.Android/MapPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Forms.Maps;
+using Android.Widget;
+using Plugin.CurrentActivity;
 
 namespace KCDriver.Droid
 {
@@ -24,6 +26,31 @@ namespace KCDriver.Droid
             KCApi.CancelRide(KCApi.Properties.CurrentRide);
             KCApi.Stop();
             return false;
+        }
+
+        public void ButtonCancelRide(object sender, EventArgs e)
+        {
+            if (!KCApi.CancelRide(KCApi.Properties.CurrentRide))
+            {
+                var text = "There was a problem with cancellation.";
+                Toast.MakeText(CrossCurrentActivity.Current.Activity, text, ToastLength.Short).Show();
+            }
+            else Navigation.PopAsync();
+        }
+
+        public void ButtonCompleteRide(object sender, EventArgs e)
+        {
+            if (!KCApi.CompleteRide(KCApi.Properties.CurrentRide))
+            {
+                var text = "There was a problem completing the ride.";
+                Toast.MakeText(CrossCurrentActivity.Current.Activity, text, ToastLength.Short).Show();
+            }
+            else Navigation.PopAsync();
+        }
+
+        public void ButtonCallRide(object sender, EventArgs e)
+        {
+            //KCApi.Properties.CurrentRide.CallClient();
         }
     }
 }
