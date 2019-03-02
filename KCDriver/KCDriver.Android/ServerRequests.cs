2d1ba1ad37790e34f5ee9dcf530b776008bdb9f7
Josh <basicjosh@gmail.com>
2019-03-01 16:13:57 -0800

    Updated the ServerRequests class to parse the server response as a Json object. Added the RecoveryCheck request to check if a driver already has a ride.

--- KCDriver/KCDriver.Android/ServerRequests.cs
+++ KCDriver/KCDriver.Android/ServerRequests.cs
@@ -1,20 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.CSharp;
 
 //class which handles calls to the server including authentication and ride requestes
 namespace KCDriver.Droid {
 
     static partial class KCApi {
 
+        private const string ip = "148.72.40.62";
+
         //authentication functions
         //returns true if the user is authenticated with the server
         public static bool Authenticate(String password, String userName) {
 
-            String message = "http://148.72.40.62/driver/auth/authenticate.php?username=" + userName + "&pwHsh=" + GetHash(password, userName);
+            string message = "http://" + ip + "/driver/auth/authenticate.php?username=" + userName + "&pwHsh=" + GetHash(password, userName);
             string responseFromServer = "";
             try {
             // Create a request for the URL. 		
@@ -31,24 +35,26 @@ namespace KCDriver.Droid {
             reader.Close();
             dataStream.Close();
             response.Close();
+            dynamic jObject = null;
+            if (!responseFromServer.Contains("Failure")) {
+                jObject = JObject.Parse(responseFromServer);
+
+                Driver_Id.driver_Id = Int32.Parse((string)jObject.result.driverID);
+                Driver_Id.token = (string)jObject.result.token;
+                Driver_Id.authenticated = true;
+
+                return true;
+            } else {
+                return false;
+                }
             } catch (Exception e) {
                 KCApi.OutputException(e);
                 return false;
             }
-
-            if (!responseFromServer.Contains("error")) {
-                String[] data = responseFromServer.Split(new char[] { '"', ',', ':' }, StringSplitOptions.RemoveEmptyEntries);
-
-                Driver_Id.driver_Id = Int32.Parse(data[4]);
-                Driver_Id.token = data[6];
-                Driver_Id.authenticated = true;
-                return true;
-            }
-
-            return false;
+                
         }
 
-        //calculates the hash of the password with the salt
+        //calculates the hash of the password with the salt.
         private static string GetHash(string input, String userName) {
             using (SHA256 sha256Hash = SHA256.Create()) {
                 HashAlgorithm hashAlgorithm = sha256Hash;
@@ -69,10 +75,10 @@ namespace KCDriver.Droid {
             }
         }
 
-        //gets the salt of a user
+        //gets the salt of a user.
         public static String GetSalt(String userName) {
-            String message = "http://148.72.40.62/driver/auth/getSalt.php?username=" + userName;
-            string responseFromServer = "";
+            String message = "http://" + ip + "/driver/auth/getSalt.php?username=" + userName;
+            String responseFromServer = "";
             try {
                 // Create a request for the URL. 		
                 WebRequest request = WebRequest.Create(message);
@@ -90,22 +96,25 @@ namespace KCDriver.Droid {
                 reader.Close();
                 dataStream.Close();
                 response.Close();
+
+                dynamic jObject = null;
+                if (!responseFromServer.Contains("Failure")) {
+                    jObject = JObject.Parse(responseFromServer);
+                    return (string)jObject.result.pwSlt;
+                } else {
+                    return "error";
+                }
             } catch (Exception e) {
                 KCApi.OutputException(e);
                 return "Error Connecting to Server";
             }
-
-            if (!responseFromServer.Contains("error")) {
-                String[] data = responseFromServer.Split(new char[] { '"', ',', ':' }, StringSplitOptions.RemoveEmptyEntries);
-                return data[4];
-            } else {
-                return "error";
-            }
+            
         }
-
+        
+        //checks if there are any rides in the queue.
         public static String CheckQueue()
         {
-            String message = "http://148.72.40.62/driver/checkQueue.php";
+            String message = "http://" + ip + "/driver/checkQueue.php";
             string responseFromServer = "";
             // Create a request for the URL. 		
             WebRequest request = WebRequest.Create(message);
@@ -124,26 +133,32 @@ namespace KCDriver.Droid {
                 reader.Close();
                 dataStream.Close();
                 response.Close();
+
+                dynamic jObject = null;
+                if (!responseFromServer.Contains("Failure")) {
+                    jObject = JObject.Parse(responseFromServer);
+                    String result = (string)jObject.result;
+                    if (result.Equals("Rides are available")) {
+                        return "Rides are available";
+                    } else if (result.Equals("No available rides")) {
+                        return "No available rides";
+                    } else if ((result.Equals("Failure"))) {
+                        return "error";
+                    } else {
+                        return "error";
+                    }
+                }
             } catch (Exception e) {
                 KCApi.OutputException(e);
                 return "Error Connecting to Server";
             }
-            //If
-
-            if (responseFromServer.Contains("Rides are available")) {
-                return "Rides are available";
-            } else if (responseFromServer.Contains("No available rides")) {
-                return "No available rides";
-            } else if ((responseFromServer.Contains("error"))) {
-                return "error";
-            } else {
-                return "error";
-            }
+            return "error";
         }
-
+        
+        //binds a driver to a ride.
         public static bool AcceptNextRide(Ride ride)
         {
-            string message = "http://148.72.40.62/driver/acceptRide.php?token=" + Driver_Id.token + "&driverID=" + Driver_Id.driver_Id;
+            string message = "http://" + ip + "/driver/acceptRide.php?token=" + Driver_Id.token + "&driverID=" + Driver_Id.driver_Id;
             // Create a request for the URL. 		
             WebRequest request = WebRequest.Create(message);
             // Get the response.
@@ -160,42 +175,34 @@ namespace KCDriver.Droid {
                 reader.Close();
                 dataStream.Close();
                 response.Close();
-            } catch (Exception e) {
-                KCApi.OutputException(e);
-                return false;
-            }
-            
-            try {
-                String[] data = responseFromServer.Split(new char[] { '"', ',', ':' }, StringSplitOptions.RemoveEmptyEntries);
-                if (!responseFromServer.Contains("error"))
-                {
-                    ride.SetRideID(Int32.Parse(data[4]));
-                    ride.SetRidePhoneNum(data[6]);
-                    ride.SetName(data[8]);
+
+                dynamic jObject = null;
+                if (!responseFromServer.Contains("Failure")) {
+                    jObject = JObject.Parse(responseFromServer);
+
+                    ride.SetRideID(Int32.Parse((string)jObject.result.rideID));
+                    ride.SetRidePhoneNum((string)jObject.result.phone);
+                    ride.SetName((string)jObject.result.name);
+                    return true;
                 }
                 //If the response comes back as Authentication failure then set the driver as not authenticated.
-                else if (responseFromServer.Contains("Authentication failure")) {
+                else if (responseFromServer.Contains("Unable to authenticate") || responseFromServer.Contains("Authentication failure")) {
                     Driver_Id.authenticated = false;
                     return false;
                 }
-                else
-                {
-                    ride.SetRideID(Int32.Parse(data[6]));
-                }
-
-                return true;
-            }
-            catch (Exception e)
-            {
+                
+                return false;
+            } catch (Exception e) {
                 KCApi.OutputException(e);
                 return false;
             }
 
         }
-
+        
+        //sets the clients postion in the ride class.
         public static bool SetRideLocation(Ride ride, double latitude, double longitude)
         {
-            string message = "http://148.72.40.62/driver/rideStatus.php?driverID=" + Driver_Id.driver_Id
+            string message = "http://" + ip + "/driver/rideStatus.php?driverID=" + Driver_Id.driver_Id
                 + "&token=" + Driver_Id.token + "&rideID=" + ride.RideId + "&lat=" + latitude + "&lon=" + longitude;
             // Create a request for the URL. 		
             WebRequest request = WebRequest.Create(message);
@@ -227,11 +234,11 @@ namespace KCDriver.Droid {
             {
                 try
                 {
-                    //If there are no errors add the lat and long to the ride class and return true.
-                    String[] data = responseFromServer.Split(new char[] { '"', ',', ':', '}' }, StringSplitOptions.RemoveEmptyEntries);
-
-                    double rideLat = Double.Parse(data[4]);
-                    double rideLong = Double.Parse(data[6]);
+                    //If there are no errors add the lat and long to the ride class and return true.\
+                    dynamic jObject = JObject.Parse(responseFromServer);
+                    
+                    double rideLat = Double.Parse((String)jObject.result.lat);
+                    double rideLong = Double.Parse((String)jObject.result.lon);
 
                     ride.SetPosition(rideLat, rideLong);
                     return true;
@@ -246,9 +253,10 @@ namespace KCDriver.Droid {
             return false;
         }
 
+        //Decouples the driver from a ride and completes it.
         public static bool CompleteRide(Ride ride)
         {
-            string message = "http://148.72.40.62/driver/completeRide.php?token=" + Driver_Id.token + "&driverID=" 
+            string message = "http://" + ip + "/driver/completeRide.php?token=" + Driver_Id.token + "&driverID=" 
                 + Driver_Id.driver_Id + "&rideID=" + ride.RideId;
 
             // Create a request for the URL. 		
@@ -274,9 +282,10 @@ namespace KCDriver.Droid {
             return false;
         }
 
+        //Sends the location of the driver to the client.
         public static bool SetDriverLocation(double latitude, double longitude)
         {
-            string message = "http://148.72.40.62/driver/updateLocation.php?driverID=" +  Driver_Id.driver_Id
+            string message = "http://" + ip + "/driver/updateLocation.php?driverID=" +  Driver_Id.driver_Id
                 + "&token=" + Driver_Id.token + "&lat=" + latitude + "&lon=" + longitude;
             string responseFromServer = "";
             try {
@@ -311,9 +320,10 @@ namespace KCDriver.Droid {
             return false;
         }
 
+        //Decouples the driver from the ride and puts the ride back in the queue.
         public static bool CancelRide(Ride ride)
         {
-            string message = "http://148.72.40.62/driver/decouple.php?" + "rideID=" + ride.RideId + "&token=" + Driver_Id.token +
+            string message = "http://" + ip + "/driver/decouple.php?" + "rideID=" + ride.RideId + "&token=" + Driver_Id.token +
                  "&driverID=" + Driver_Id.driver_Id;
             string responseFromServer = "";
             // Create a request for the URL. 		
@@ -343,6 +353,46 @@ namespace KCDriver.Droid {
             }
 
             return false;
+        }
+
+        //If the driver already has a ride it sets the ride id to that ride.
+        public static bool RecoveryCheck(Ride ride) {
+            string message = "http://" + ip + "/driver/recoveryCheck.php?token=" + Driver_Id.token + "&driverID=" + Driver_Id.driver_Id;
+            // Create a request for the URL. 		
+            WebRequest request = WebRequest.Create(message);
+            // Get the response.
+            string responseFromServer = "";
+            try {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                responseFromServer = reader.ReadToEnd();
+                // Cleanup the streams and the response.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                dynamic jObject = null;
+                if (!responseFromServer.Contains("Failure")) {
+                    jObject = JObject.Parse(responseFromServer);
+
+                    ride.SetRideID(Int32.Parse((string)jObject.result.rideID));
+                    return true;
+                }
+                //If the response comes back as Authentication failure then set the driver as not authenticated.
+                else if (responseFromServer.Contains("Authentication failure")) {
+                    Driver_Id.authenticated = false;
+                    return false;
+                }
+
+                return false;
+            } catch (Exception e) {
+                KCApi.OutputException(e);
+                return false;
+            }
         }
     }
 }
